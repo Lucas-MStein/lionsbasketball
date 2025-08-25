ec79962a39c4c0d7ae1262daf02a43f8d160c8ed
Lucas-Maurice Stein <lms@koempf24.de>
2025-08-25 10:54:31 +0200

    adjusted stuff for vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
@@ -6,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter basename="/lionsbasketball">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/', // ✅ GitHub Pages Subpfad
     plugins: [react()],
 });
