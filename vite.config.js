23b30e765714ee99985d7b9ef6ba573fe6ff5adf
Lucas-Maurice Stein <lms@koempf24.de>
2025-08-25 10:43:15 +0200

    adjusted vite.config.js for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/lionsbasketball/', // ✅ GitHub Pages Subpfad
+    base: '/', // ✅ GitHub Pages Subpfad
     plugins: [react()],
 });
